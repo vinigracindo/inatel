0d370160ebe3c073e4845397234cbf12a975efae
Vinnicyus Gracindo <vini.gracindo@gmail.com>
2022-02-08 15:22:24 -0300

    add logout

--- inatel/urls.py
+++ inatel/urls.py
@@ -25,6 +25,7 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='login.html',
          redirect_authenticated_user=True),
          name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='/login/'), name='logout'),
     path('admin/', admin.site.urls),
 ]
 
