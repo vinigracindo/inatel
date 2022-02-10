a4d9ef25c3d7a170745c3d8590e5fcf673974e6d
Vinnicyus Gracindo <vini.gracindo@gmail.com>
2022-02-10 14:36:55 -0300

    import and view file

--- core/models.py
+++ core/models.py
@@ -1,12 +1,47 @@
+from datetime import datetime
+
 from django.db import models
 
 
 class ScheduleWorkFile(models.Model):
+    REGISTRATION = 'Matrícula'
+    TECHNICIAN = 'Técnico'
+    DESCRIPTION = 'Descrição da Atividade'
+    DATE = 'Data'
+    TIME = 'Hora'
+
+    COLUMNS = [REGISTRATION, TECHNICIAN, DESCRIPTION, DATE, TIME]
+
     file = models.FileField(upload_to='imports')
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def save_schedules(self, dataframe):
+        schedules = []
+
+        for item in dataframe.index:
+            validated_dict = dataframe.loc[item].to_dict()
+
+            date = '{} {}'.format(
+                validated_dict[ScheduleWorkFile.DATE], validated_dict[ScheduleWorkFile.TIME])
+
+            schedules.append(
+                ScheduleWork(
+                    file=self,
+                    technician_name=validated_dict[ScheduleWorkFile.TECHNICIAN],
+                    technician_register=str(
+                        validated_dict[ScheduleWorkFile.REGISTRATION]),
+                    description=validated_dict[ScheduleWorkFile.DESCRIPTION],
+                    date=datetime.strptime(date, "%d/%m/%Y %H:%M:%S")
+                )
+            )
+
+        ScheduleWork.objects.bulk_create(schedules)
 
 
 class ScheduleWork(models.Model):
     file = models.ForeignKey(ScheduleWorkFile, on_delete=models.CASCADE)
-    informations = models.JSONField()
+    technician_name = models.CharField(max_length=127)
+    technician_register = models.CharField(max_length=32)
+    description = models.TextField()
+    date = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
--- core/utils.py
+++ core/utils.py
@@ -1,15 +1,24 @@
 from django.forms import ValidationError
 import pandas as pd
 
+from core.models import ScheduleWorkFile
 
-def extract_data(file):
+
+def extract_and_validate_data_from_file(file):
     if file.name.endswith('.xlsx'):
         read_file = pd.read_excel(file)
     elif file.name.endswith('.csv'):
         read_file = pd.read_csv(file, encoding='utf8')
     else:
-        return None
+        raise Exception(
+            "Formato inválido. Verifique se o formato do arquivo é csv ou xlsx.")
 
     df = pd.DataFrame(read_file)
 
-    return df
+    headers = list(df)
+
+    if not (set(ScheduleWorkFile.COLUMNS) <= set(headers)):
+        raise Exception("Colunas inválidas. Verifique se o arquivo contém as seguintes colunas: {}".format(
+            ScheduleWorkFile.COLUMNS))
+
+    return df[df.columns.unique()]
